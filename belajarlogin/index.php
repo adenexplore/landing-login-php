663b1e2e16098d7e5a4abc5c22249bf93b5b8707
Aden Rifai <adenrifai01@gmail.com>
2023-03-22 23:54:22 +0700

    first name

---
+++ belajarlogin/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>halaman pertama</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+   		<header>
+   			<ul>
+   				<a href="index.php"><li>beranda</li></a>
+   				<a href="profil.php"><li>profil</li></a>
+   				<a href="logout.php"><li>logout</li></a>
+   			</ul>
+   		</header>
+   		<section>
+   			<center><font style="font-size: 40px;">selamat datang dihalaman beranda</font></center>
+   			
+   		</section>
+   		<footer>
+   			<center><font style="font-size: 16px;">copyright &copy; ADEN AHMAD RIFAI.</font></center>
+   			
+   		</footer>
+</body>
+</html>
---
+++ belajarlogin/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>halaman pertama</title>
+   <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+   <div class="box-login">
+       <form style=" font-size:26px;color:white;">masukkan email</form>
+      <form action="" method="post">
+         <input type="text" nama= "email" placeholder="masukan email"><br>
+         <input type="password" nama= "password" placeholder="masukan password"><br>
+         <input type="submit" nama= "masuk" value="masuk"><br>
+      </form>
+   </div>
+</body>
+</html>
